52a519425277e771828f6f238ab35fd1258b3a3e
GroovieGermanikus <grooviegermanikus@gmail.com>
2026-04-28 18:01:19 +0200

    fix compile issue in mock crate

--- crates/mock/src/lib.rs
+++ crates/mock/src/lib.rs
@@ -64,7 +64,7 @@ impl From<AccountInfo> for SubscribeUpdateAccount {
                 txn_signature: None,
                 write_version: 0,
                 pubkey: value.pubkey.to_bytes().to_vec(),
-                data: value.data,
+                data: value.data.into(),
                 executable: value.executable,
                 lamports: value.lamports,
                 owner: value.owner.to_bytes().to_vec(),
@@ -96,7 +96,7 @@ impl TryFrom<SubscribeUpdateAccount> for AccountInfo {
 
         Ok(Self {
             pubkey,
-            data: account_info.data.clone(),
+            data: account_info.data.to_vec(),
             executable: account_info.executable,
             lamports: account_info.lamports,
             owner,
